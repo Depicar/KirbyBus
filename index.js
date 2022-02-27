cd54989909921d8c37edde1b15addd3dc29c2a86
Charlie Zhong <alwayscharlieman@gmail.com>
2022-02-27 17:43:51 -0500

    removed duplicate code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const axios = require('axios');
 require('dotenv').config();
+
 const token = process.env.TOKEN;
 const key = process.env.KEY;
 
@@ -10,11 +11,18 @@ const client = new Discord.Client({
 
 const prefix = '';
 
+//command map
+const busNames = new Map([
+    ["bbaits", 'BB'],
+    ["northwood", 'NW'],
+  ]);
 
+//ready check
 client.once('ready', () => {
     console.log('KirbyBus GOOD');
 });
 
+//message comands
 client.on('message', async message => {
     if(!message.content.startsWith(prefix) || message.author.bot) return;
     
@@ -27,9 +35,9 @@ client.on('message', async message => {
         message.channel.send('pong!');
     }
 
-    if(command === 'bbaits') {
+    if(command === 'bbaits' || command === 'northwood') {
         let getBuses = async () => {
-            let response = await axios.get(`https://mbus.ltp.umich.edu/bustime/api/v3/getvehicles?key=${key}&rt=BB&format=json`);
+            let response = await axios.get(`https://mbus.ltp.umich.edu/bustime/api/v3/getvehicles?key=${key}&rt=${busNames.get(command)}&format=json`);
 
             //find all vehicles
             let vehicles = response['data']['bustime-response']['vehicle'];
@@ -57,51 +65,12 @@ client.on('message', async message => {
             message.channel.send(`There aren't any ${command} buses running right now.`);
         }
         else {
-            message.channel.send("Your Bursley Baits buses are at locations: ");
+            message.channel.send(`Your ${command} buses are at locations: `);
             for (let i = 0; i < buses.length; i++) {
                 message.channel.send(`${buses[i]}`);
             }
         }
         
-        
-    }
-
-//same code as above, trying to figure out how to do it based on whatever the input is so i don't have to copy
-    if(command === 'northwood') {
-        let getBus = async () => {
-            let response = await axios.get(`https://mbus.ltp.umich.edu/bustime/api/v3/getvehicles?key=${key}&rt=NW&format=json`);
-
-            //find all vehicles
-            let vehicles = response['data']['bustime-response']['vehicle'];
-
-            //check if there are no buses running
-            if (vehicles === undefined) {
-                return 1;
-            }
-
-            var size = Object.keys(vehicles).length;
-
-            //log all vehicle locations
-            let locations = [];
-            for (let i = 0; i < size; i++) {
-                locations.push([vehicles[i]['lat'], vehicles[i]['lon']]);
-            }
-
-            return locations;
-
-        }
-        let buses = await getBus();
-
-        //check if any buses are actually running
-        if (buses === 1) {
-            message.channel.send(`There aren\'t any ${command} buses running right now.`);
-        }
-        else {
-            message.channel.send("Your Northwood buses are at locations: ");
-            for (let i = 0; i < buses.length; i++) {
-                message.channel.send(`${buses[i]}`);
-            }
-        }
         
     }
 }
