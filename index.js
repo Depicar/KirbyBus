14bd93ed7065285729ca63aafbf8616ef466c049
Charlie Zhong <alwayscharlieman@gmail.com>
2022-03-01 23:19:46 -0500

    get bus time function, set home locations increased

--- index.js
+++ index.js
@@ -19,14 +19,15 @@ const busLines = new Map([
   ]);
 
 //common stops map (might be able to move to separate file)
-const busStops = new Map([
-    ["cctc", [42.278176000001, -83.735083999999]],
-    ["bursley", [42.294595999999, -83.72071]],
-    ["bursley outbound", [42.294697000001, -83.721364]],
+const busStopIds = new Map([
+    ["cctcmuseum", "C251",],
+    ["cctcchem", "C250"],
+    ["bursleyin", "N407"],
+    ["bursleyout", "N408"],
 ]);
 
 //user current location
-var home = 'cctc';
+var home = 'cctcmuseum';
 
 
 //ready check
@@ -48,7 +49,7 @@ client.on('message', async message => {
     //set home location 
     if(command === 'sethome') {
         //check if valid location, args.length check for when user types just 'sethome'
-        if (args.length === 0 || !busStops.has(args[0].toLowerCase())) {
+        if (args.length === 0 || !busStopIds.has(args[0].toLowerCase())) {
             message.channel.send('Invalid bus stop.');
             return;
         }
@@ -66,24 +67,52 @@ client.on('message', async message => {
             return;
         }   
 
-        var busLine = args[0].toLowerCase();
+        var inputBusLine = args[0].toLowerCase();
 
-        //TODO: implement prediction function
-        if (args[1] && args[1] == 'time') {
+        //predicts how long it takes for closest bus to get to home
+        if (args[1] && args[1].toLowerCase() === 'time') {
+
+            let getPrediction = async () => {
+                let response = await axios.get(`https://mbus.ltp.umich.edu/bustime/api/v3/getpredictions?key=${key}&rt=${busLines.get(inputBusLine)}&stpid=${busStopIds.get(home)}&format=json`)
+                let prediction = response['data']['bustime-response']['prd']
+
+                //for buses not running
+                if (prediction === undefined) {
+                    return -1;
+                }
+
+                return prediction[0]['prdctdn'];
+            }
+
+            let predictionByMin = await getPrediction();
+
+            //outside check for buses not running
+            if (predictionByMin === -1) {
+                message.channel.send(`There aren't any ${busLines.get(inputBusLine)} buses running right now.`);
+                return;
+            }
+            
+            //special case for 'DUE' response
+            if (predictionByMin === 'DUE') {
+                message.channel.send(`The ${busLines.get(inputBusLine)} bus is DUE! RUN!`);
+                return;
+            }
+
+            message.channel.send(`A ${busLines.get(inputBusLine)} bus is coming to you in ${predictionByMin} minutes!`);
 
             return;
         }
 
         //get bus locations
         let getBuses = async () => {
-            let response = await axios.get(`https://mbus.ltp.umich.edu/bustime/api/v3/getvehicles?key=${key}&rt=${busLines.get(busLine)}&format=json`);
+            let response = await axios.get(`https://mbus.ltp.umich.edu/bustime/api/v3/getvehicles?key=${key}&rt=${busLines.get(inputBusLine)}&format=json`);
 
             //find all vehicles
             let vehicles = response['data']['bustime-response']['vehicle'];
             
             //check if there are no buses running
             if (vehicles === undefined) {
-                return 1;
+                return -1;
             }
 
             var size = Object.keys(vehicles).length;
@@ -100,11 +129,11 @@ client.on('message', async message => {
         let buses = await getBuses();
 
         //outside check if any buses are actually running
-        if (buses === 1) {
-            message.channel.send(`There aren't any ${busLines.get(busLine)} buses running right now.`);
+        if (buses === -1) {
+            message.channel.send(`There aren't any ${busLines.get(inputBusLine)} buses running right now.`);
         }
         else {
-            message.channel.send(`Your ${busLines.get(busLine)} buses are at locations: `);
+            message.channel.send(`Your ${busLines.get(inputBusLine)} buses are at locations: `);
             for (let i = 0; i < buses.length; i++) {
                 message.channel.send(`${buses[i]}`);
             }
