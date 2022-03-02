0e3e7f130fc9497cb3e5cb9d3c899caf2f6d13b8
Charlie Zhong <alwayscharlieman@gmail.com>
2022-03-01 22:12:57 -0500

    added sethome command for future, rebuilt command structure now all bus inquiries start with bus and then the buslines, more invalid input bot responses and ignore letter case, bus time prediction stub

--- index.js
+++ index.js
@@ -9,35 +9,74 @@ const client = new Discord.Client({
     intents: ["GUILDS", "GUILD_MESSAGES"]
 });
 
+//command prefix
 const prefix = '';
 
-//command map
-const busNames = new Map([
+//bus lines map (might be able to move to separate file)
+const busLines = new Map([
     ["bbaits", 'BB'],
     ["northwood", 'NW'],
   ]);
 
+//common stops map (might be able to move to separate file)
+const busStops = new Map([
+    ["cctc", [42.278176000001, -83.735083999999]],
+    ["bursley", [42.294595999999, -83.72071]],
+    ["bursley outbound", [42.294697000001, -83.721364]],
+]);
+
+//user current location
+var home = 'cctc';
+
+
 //ready check
 client.once('ready', () => {
     console.log('KirbyBus GOOD');
+    
 });
 
 //message comands
 client.on('message', async message => {
     if(!message.content.startsWith(prefix) || message.author.bot) return;
     
-    const args = message.content.slice(prefix.length).split(/ + /);
+    //takes first message and splits into array
+    const args = message.content.slice(prefix.length).split(/ +/);
 
     //accounts for capital letters in command
     const command = args.shift().toLowerCase();
 
-    if(command === 'ping') {
-        message.channel.send('pong!');
+    //set home location 
+    if(command === 'sethome') {
+        //check if valid location, args.length check for when user types just 'sethome'
+        if (args.length === 0 || !busStops.has(args[0].toLowerCase())) {
+            message.channel.send('Invalid bus stop.');
+            return;
+        }
+
+        home = args[0].toLowerCase();
+        message.channel.send(`Your home has been set to ${home}!`);
+        return;
     }
 
-    if(command === 'bbaits' || command === 'northwood') {
+
+    if(command === 'bus') {
+        //check if valid bus line, args.length check for when user types just 'bus'
+        if (args.length === 0 || !busLines.has(args[0].toLowerCase())) {
+            message.channel.send('Invalid bus line.');
+            return;
+        }   
+
+        var busLine = args[0].toLowerCase();
+
+        //TODO: implement prediction function
+        if (args[1] && args[1] == 'time') {
+
+            return;
+        }
+
+        //get bus locations
         let getBuses = async () => {
-            let response = await axios.get(`https://mbus.ltp.umich.edu/bustime/api/v3/getvehicles?key=${key}&rt=${busNames.get(command)}&format=json`);
+            let response = await axios.get(`https://mbus.ltp.umich.edu/bustime/api/v3/getvehicles?key=${key}&rt=${busLines.get(busLine)}&format=json`);
 
             //find all vehicles
             let vehicles = response['data']['bustime-response']['vehicle'];
@@ -62,10 +101,10 @@ client.on('message', async message => {
 
         //outside check if any buses are actually running
         if (buses === 1) {
-            message.channel.send(`There aren't any ${command} buses running right now.`);
+            message.channel.send(`There aren't any ${busLines.get(busLine)} buses running right now.`);
         }
         else {
-            message.channel.send(`Your ${command} buses are at locations: `);
+            message.channel.send(`Your ${busLines.get(busLine)} buses are at locations: `);
             for (let i = 0; i < buses.length; i++) {
                 message.channel.send(`${buses[i]}`);
             }
